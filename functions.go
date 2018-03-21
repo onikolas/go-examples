5e6504f152552a6d82257af8dd4ae9f8b6237e41
nikolas.ladas <nikolas.ladas@gmail.com>
2018-03-21 17:30:03 -0400

    functions

---
+++ functions.go
@@ -0,0 +1,27 @@
+package main 
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println(HashNow(7634, 901))
+}
+
+func HashNow(val, key int) int {
+  hashed := val % key
+  return hashed
+}
+
+// the shorter the better !
+func Hash(val, key int) int {
+  return val % key
+}
+
+// useful ?
+func HashNah(val, key int) (result int) {
+  result = val % key
+  return 
+}
+
+// what happens when you call fmt.Println(HashNow(666, 0)) ?

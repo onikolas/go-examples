270b19d726b3497f4065f09c1d88c7c6869818f7
nikolas.ladas <nikolas.ladas@gmail.com>
2018-03-21 17:00:28 -0400

    variables

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+  var aVar int
+  anotherVar := 9.0
+  fmt.Println("hi, these are the variables: ", aVar, anotherVar)
+   
+  // uncommenting any of these causes an error
+  
+  //var aVar float32
+  //aVar := 5  
+  //aVar = anotherVar
+}

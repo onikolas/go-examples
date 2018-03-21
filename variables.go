bafa7d147512b13842efb4a12320fb2acbb0be4b
nikolas.ladas <nikolas.ladas@gmail.com>
2018-03-21 17:09:50 -0400

    modified variables.go

--- variables.go
+++ variables.go
@@ -13,4 +13,10 @@ func main() {
   //var aVar float32
   //aVar := 5  
   //aVar = anotherVar
+  //AVar := 10
+  
+  var a, b, c int = 1, 3, 4
+  d, e, f := "text is good ", 4, " you"
+    
+  fmt.Println(a,b,c,d,e,f)
 }

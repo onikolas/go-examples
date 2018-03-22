342f0ebb58e62fb6096df4947025069e3c19fe1a
nikolas <nikolas@my_archi>
2018-03-22 21:10:58 +0200

    loops

---
+++ loop.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	i := 0
+	for i < 10 {
+		fmt.Print(i, " ")
+		i = i + 1
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i, " ")
+	}
+
+	for {
+		fmt.Println("Control+C ! ")
+	}
+}
+
+// Write for loops to produce the following outputs:
+
+// ******
+// ******
+// ******
+// ******
+// ******
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+
+// ******
+// ****
+// **
+
+// 1
+// 12
+// 1234
+// 12345678
+
+// ******
+// *    *
+// *    *
+// *    *
+// ******
--- variables.go
+++ variables.go
@@ -3,20 +3,22 @@ package main
 import "fmt"
 
 func main() {
-  
-  var aVar int
-  anotherVar := 9.0
-  fmt.Println("hi, these are the variables: ", aVar, anotherVar)
-   
-  // uncommenting any of these causes an error
-  
-  //var aVar float32
-  //aVar := 5  
-  //aVar = anotherVar
-  //AVar := 10
-  
-  var a, b, c int = 1, 3, 4
-  d, e, f := "text is good ", 4, " you"
-    
-  fmt.Println(a,b,c,d,e,f)
+
+	var aVar int
+	anotherVar := 9.0
+	fmt.Println("hi, these are the variables: ", aVar, anotherVar)
+
+	// uncommenting any of these causes an error
+
+	//var aVar float32
+	//aVar := 5
+	//aVar = anotherVar
+	//AVar := 10
+
+	var a, b, c int = 1, 3, 4
+	d, e, f := "text is good ", 4, " you "
+
+	fmt.Println(a, b, c, d, e, f, PI)
 }
+
+const PI = 3.14159265359

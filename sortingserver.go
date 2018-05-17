e80d49f06e3137efb3f480c2a94851e9697c6d51
nikolas <nikolas@my_archi>
2018-05-18 00:00:32 +0300

    Added roadmap

--- sortingserver.go
+++ sortingserver.go
@@ -1,9 +1,7 @@
-// Implement sortie.
-
-// Sortie accepts data from various sources and sorts them. On request, it outputs the sorted data to
-// either the screen or a file. We have outside guarantees that our data will be dense so counting sort
-// makes sense as the underlying algorithm. Sortie must be able to sort any data that implements the
-// Sorter interface (we must define what this is).
+// Sorting server accepts data from various sources and sorts them. On request, it outputs the sorted
+// data to  either the screen or a file. We have outside guarantees that our data will be dense so
+// counting sort  makes sense as the underlying algorithm. Sortie must be able to sort any data
+// that implements the  Sorter interface (we must define what this is).
 //
 // Can accept data from:
 // -A file, given at program invocation.
@@ -17,6 +15,23 @@
 /* [ [1,2,3,4,5],
    [1,3,3,4,4],
    [1,1,1,1,1] ]
+*/
+
+/*
+Road map:
+========
+1. Define a CountSort data type. It should be safe for concurrent access. Only sorts ints.
+2. Add methods to CountSort:
+   Initialize() : sets array length
+   Add() : adds a number to the data
+   Sort() : sorts and returns a sorted array
+3. Spawn a goroutine that periodically Add()s data to CountSort (this simulates the network)
+4. Spawn a goroutine that listens for a specific keypress (e.g letter 'p') after which it sorts
+   and prints the data
+5. Spawn multiple Add()ers that push data concurrently
+6. Can we optimize? Are there operations that can be done in parallel?
+7. Generalize (interface) so that we can sort other data.
+8. Replace step 3 with network
 */
 
 package main
@@ -33,7 +48,7 @@ import (
 
 func main() {
 	go FileSorter("sortingexample.csv")
-	time.Sleep(time.Second*2)
+	time.Sleep(time.Second * 2)
 }
 
 func FileSorter(f string) []int {
@@ -52,12 +67,12 @@ func ContinuousSorter(f string) []int {
 func DataCleaner(inputData []string) []int {
 	var destination []int
 	for i := range inputData {
-			v := inputData[i]
-			w, err := strconv.Atoi(v)
-			if err != nil {
-				log.Printf("Input %v from the input file is not an integer", i)
-			}
-			destination = append(destination, w)
+		v := inputData[i]
+		w, err := strconv.Atoi(v)
+		if err != nil {
+			log.Printf("Input %v from the input file is not an integer", i)
+		}
+		destination = append(destination, w)
 	}
 	return destination
 }
@@ -95,30 +110,30 @@ func ConvertToSlice(inputData [][]string) []string {
 
 // CountSort sorts an integer slice (https://en.wikipedia.org/wiki/Counting_sort)
 func CountSort(input []int) []int {
-  sorted := make([]int, len(input))
-  size := input[0]
+	sorted := make([]int, len(input))
+	size := input[0]
 
-  // find the size
-  for i := range input {
-    if input[i] > size {
-      size = input[i]
-    }
-  }
+	// find the size
+	for i := range input {
+		if input[i] > size {
+			size = input[i]
+		}
+	}
 
-  // populate index array
-  auxArray := make([]int, size+1)
-  for i := range input {
-    auxArray[input[i]]++
-  }
+	// populate index array
+	auxArray := make([]int, size+1)
+	for i := range input {
+		auxArray[input[i]]++
+	}
 
-  // save the sorted results
-  count := 0
-  for i := range auxArray {
-    for j := 0; j < auxArray[i]; j++ {
-      sorted[count] = i
-      count++
-    }
-  }
+	// save the sorted results
+	count := 0
+	for i := range auxArray {
+		for j := 0; j < auxArray[i]; j++ {
+			sorted[count] = i
+			count++
+		}
+	}
 
-  return sorted
+	return sorted
 }
